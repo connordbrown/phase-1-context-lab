521fb2fc0e2c41ba8085bb66c102806a3426da3c
Connor <connordbrown@gmail.com>
2023-08-14 21:08:52 -0600

    completes lab

--- index.js
+++ index.js
@@ -21,3 +21,61 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(employeeArr) {
+    return {
+            firstName: employeeArr[0],
+            familyName: employeeArr[1],
+            title: employeeArr[2],
+            payPerHour: employeeArr[3],
+            timeInEvents: [],
+            timeOutEvents: []
+           }
+}
+
+function createEmployeeRecords(employeesArr) {
+    return employeesArr.map(employeeArr => createEmployeeRecord(employeeArr))
+}
+
+function createTimeInEvent(dateTime) {
+    this.timeInEvents.push({
+                            type: "TimeIn",
+                            date: dateTime.slice(0, 10),
+                            hour: parseInt(dateTime.slice(11))
+                            });
+    return this;
+}
+
+function createTimeOutEvent(dateTime) {
+    this.timeOutEvents.push({
+                             type: "TimeOut",
+                             date: dateTime.slice(0, 10),
+                             hour: parseInt(dateTime.slice(11))
+                            });
+    return this;
+}
+
+function hoursWorkedOnDate(workDate) {
+    const timeOut = this.timeOutEvents.find((element) => element.date === workDate).hour;
+    const timeIn = this.timeInEvents.find((element) => element.date === workDate).hour;
+
+    return (timeOut - timeIn)/100;
+}
+
+function wagesEarnedOnDate(workDate) {
+    return this.payPerHour * hoursWorkedOnDate.call(this, workDate);
+}
+
+function findEmployeeByFirstName(collection, firstNameString) {
+    return collection.find((employee) => employee.firstName === firstNameString);
+}
+
+function calculatePayroll(employees) {
+    const wages = employees.map((employee) => allWagesFor.call(employee)); // employee is context
+    const payroll = wages.reduce((total, wage) => {
+        total += wage;
+        return total;
+    }, 0);
+    return payroll;
+}
+
+
